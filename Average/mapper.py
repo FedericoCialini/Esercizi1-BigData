22be3c7fe64a71049535e199a28f9becf75a5650
federico <federicocialini@gmail.com>
2020-03-27 12:40:18 +0100

    average mapper

---
+++ Average/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""mapper.py"""
+import sys
+from builtins import print
+
+doc = sys.stdin.read()
+doc = doc.strip()
+doc = doc.split()
+for word in doc:
+    print('%s\t%s' % (word[0],str(len(word))))
